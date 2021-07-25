af7a7dfab82d31b4ac1a74fd5a1eb01f801bb88f
seiya endo <seiya.endo0830@gmail.com>
2021-07-25 19:46:02 +0900

    php 用 ecr の作成

--- envs/prod/app/foobar/ecr.tf
+++ envs/prod/app/foobar/ecr.tf
@@ -2,3 +2,8 @@ module "nginx" {
   source = "../../../../modules/ecr"
   name   = "laravel-fargate-infra-prod-foobar-nginx"
 }
+
+module "php" {
+  source = "../../../../modules/ecr"
+  name   = "example-prod-foobar-php"
+}
