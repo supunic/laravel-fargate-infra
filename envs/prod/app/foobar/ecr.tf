c4f14300203f605addb6e1793b5dfb1a9c99d159
seiya endo <seiya.endo0830@gmail.com>
2021-07-25 19:44:55 +0900

    ecr.tf 不要コードの削除

--- envs/prod/app/foobar/ecr.tf
+++ envs/prod/app/foobar/ecr.tf
@@ -1,32 +1,3 @@
-resource "aws_ecr_repository" "nginx" {
-  name = "laravel-fargate-infra-prod-foobar-nginx"
-  tags = {
-    Name = "laravel-fargate-infra-prod-foobar-nginx"
-  }
-}
-
-resource "aws_ecr_lifecycle_policy" "nginx" {
-  policy = jsonencode(
-    {
-      "rules" : [
-        {
-          "rulePriority" : 1,
-          "description" : "Hold only 10 images, expire all others",
-          "selection" : {
-            "tagStatus" : "any",
-            "countType" : "imageCountMoreThan",
-            "countNumber" : 10
-          },
-          "action" : {
-            "type" : "expire"
-          }
-        }
-      ]
-    }
-  )
-  repository = aws_ecr_repository.nginx.name
-}
-
 module "nginx" {
   source = "../../../../modules/ecr"
   name   = "laravel-fargate-infra-prod-foobar-nginx"
