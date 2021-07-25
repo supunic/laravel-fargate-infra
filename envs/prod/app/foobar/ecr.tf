1f0fdb527375894d35c40197d990b9d7e8389ad1
seiya endo <seiya.endo0830@gmail.com>
2021-07-26 08:38:11 +0900

    Local Values の利用

--- envs/prod/app/foobar/ecr.tf
+++ envs/prod/app/foobar/ecr.tf
@@ -1,9 +1,9 @@
 module "nginx" {
   source = "../../../../modules/ecr"
-  name   = "laravel-fargate-infra-prod-foobar-nginx"
+  name   = "${local.name_prefix}-prod-${local.service_name}-nginx"
 }
 
 module "php" {
   source = "../../../../modules/ecr"
-  name   = "laravel-fargate-infra-prod-foobar-php"
+  name   = "${local.name_prefix}-prod-${local.service_name}-php"
 }
